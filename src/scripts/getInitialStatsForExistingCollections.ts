cdb580bb3b918222f1c224cba057e6da9ff6e7b6
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-03-28 21:21:49 -0500

    minor

--- src/scripts/getInitialStatsForExistingCollections.ts
+++ src/scripts/getInitialStatsForExistingCollections.ts
@@ -3,30 +3,38 @@ import { firebase, logger } from 'container';
 import { addCollectionToQueue, taskQueue } from 'controllers/sales-collection-initializer.controller';
 
 export async function getInitialStatsForExistingCollections() {
-  const collectionsQuery = firebase.db.collection(firestoreConstants.COLLECTIONS_COLL);
+  try {
+    const collectionsQuery = firebase.db.collection(firestoreConstants.COLLECTIONS_COLL);
 
-  const stream = collectionsQuery.stream();
+    const stream = collectionsQuery.stream();
 
-  setInterval(() => {
-    logger.log(`Queue Size: ${taskQueue.size + taskQueue.pending}`);
-  }, 5000);
+    const interval = setInterval(() => {
+      const size = taskQueue.size + taskQueue.pending;
+      logger.log(`Initializing stats for: ${size} collections`);
 
-  let count = 0;
-  for await (const docSnap of stream) {
-    try {
-      const docSnapshot = docSnap as any as FirebaseFirestore.QueryDocumentSnapshot;
-      const allTimeStats = docSnapshot.ref.collection(firestoreConstants.COLLECTION_STATS_COLL).doc('all');
-      const allTimeStatsSnap = await allTimeStats.get();
-      const stats = allTimeStatsSnap.data();
-      if (!stats) {
-        count += 1;
-        logger.log(`No stats found for collection ${docSnapshot.id}`);
-        const [chainId, address] = docSnapshot.ref.id.split(':');
-        void addCollectionToQueue(address, '1', chainId);
+      if (size === 0) {
+        clearInterval(interval);
       }
-    } catch (err) {
-      logger.error(err);
+    }, 5000);
+
+    let count = 0;
+    for await (const docSnap of stream) {
+      try {
+        const docSnapshot = docSnap as any as FirebaseFirestore.QueryDocumentSnapshot;
+        const allTimeStats = docSnapshot.ref.collection(firestoreConstants.COLLECTION_STATS_COLL).doc('all');
+        const allTimeStatsSnap = await allTimeStats.get();
+        const stats = allTimeStatsSnap.data();
+        if (!stats) {
+          count += 1;
+          const [chainId, address] = docSnapshot.ref.id.split(':');
+          void addCollectionToQueue(address, '1', chainId);
+        }
+      } catch (err) {
+        logger.error(err);
+      }
     }
+    logger.log(`Found: ${count} collections without stats`);
+  } catch (err) {
+    logger.error('failed to get initial stats for existing collections', err);
   }
-  logger.log(`Found: ${count} collections without stats`);
 }
