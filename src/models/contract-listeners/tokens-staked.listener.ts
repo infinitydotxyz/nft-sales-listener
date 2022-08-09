af630838bb2500d412f073fef1a6cad8c2b233d5
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-08-09 13:42:10 -0500

    added tokens staked listener

---
+++ src/models/contract-listeners/tokens-staked.listener.ts
@@ -0,0 +1,65 @@
+import { ChainId } from '@infinityxyz/lib/types/core/ChainId';
+import { trimLowerCase } from '@infinityxyz/lib/utils';
+import { BigNumber, ethers } from 'ethers';
+import { BlockProvider } from '../block-provider';
+import { ContractListener, Events } from './contract-listener.abstract';
+
+export enum StakeDuration {
+  None = 'NONE',
+  ThreeMonths = 'THREE_MONTHS',
+  SixMonths = 'SIX_MONTHS',
+  TwelveMonths = 'TWELVE_MONTHS'
+}
+
+const contractStakeDurationToEnum: Record<number, StakeDuration > = {
+    [0]: StakeDuration.None,
+    [1]: StakeDuration.ThreeMonths,
+    [2]: StakeDuration.SixMonths,
+    [3]: StakeDuration.TwelveMonths
+}
+
+export type TokensStakedEvent = {
+  user: string;
+  amount: string;
+  duration: StakeDuration;
+  blockNumber: number;
+  txHash: string;
+};
+
+export class TokensStakedListener extends ContractListener<TokensStakedEvent, Events<TokensStakedEvent>> {
+  public readonly eventName = 'Staked';
+  protected _eventFilter: ethers.EventFilter;
+
+  constructor(contract: ethers.Contract, blockProvider: BlockProvider, chainId: ChainId) {
+    super(contract, blockProvider, chainId);
+    this._eventFilter = contract.filters.Staked();
+  }
+
+  decodeLog(args: ethers.Event[]): TokensStakedEvent | null {
+    if (!args?.length || !Array.isArray(args) || !args[args.length - 1]) {
+      return null;
+    }
+    const event: ethers.Event = args[args.length - 1];
+    const eventData = event.args;
+    if (eventData?.length !== 3) {
+      return null;
+    }
+
+    // see commented reference below for payload structure
+    const user = trimLowerCase(String(eventData[0]));
+    const amount = BigNumber.from(String(eventData[1])).toString();
+    const eventDuration = parseInt(String(eventData[2]));
+    const duration = contractStakeDurationToEnum[eventDuration] ?? null;
+    if(duration == null) {
+        console.warn(`Unknown duration: ${eventDuration}`);
+        return null;
+    }
+    return {
+        user,
+        amount,
+        duration,
+        blockNumber: event.blockNumber,
+        txHash: event.transactionHash
+    }
+  }
+}
